187a4afd7f2645c0f5bfffc4d67e1557ec0d97ad
Mubarak <dewriter.sam@gmail.com>
2025-05-24 13:41:46 +0100

    Add SQL files

---
+++ sql/create_tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS students (
+    student_id VARCHAR(32) PRIMARY KEY,,
+    full_name VARCHAR(100) NOT NULL,
+    math_score FLOAT,
+    english_score FLOAT,
+    science_score FLOAT,
+    attendance_rate FLOAT,
+    term VARCHAR(10),
+    gpa FLOAT,
+    risk_score FLOAT,
+    engagement_level VARCHAR(20), 
+);
+
+CREATE TABLE at_risk_students (
+    student_id VARCHAR(32) PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    risk_score FLOAT NOT NULL,
+    engagement_level VARCHAR(20),
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
+
+CREATE TABLE IF NOT EXISTS top_students (
+    student_id VARCHAR(32) PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    gpa FLOAT NOT NULL,
+    gender VARCHAR(10),
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
